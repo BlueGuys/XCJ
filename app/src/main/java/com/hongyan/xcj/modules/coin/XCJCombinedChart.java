03ba9f13bcfda47523d0aa099d650dcae135a66f
王宁 <wangninga@jiedaibao.com>
2018-03-27 21:33:44 +0800

    Fix K线图增加

---
+++ app/src/main/java/com/hongyan/xcj/modules/coin/XCJCombinedChart.java
@@ -0,0 +1,26 @@
+package com.hongyan.xcj.modules.coin;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.github.mikephil.charting.charts.CombinedChart;
+
+/**
+ * Created by wangning on 2018/3/27.
+ */
+
+public class XCJCombinedChart extends CombinedChart {
+
+    public XCJCombinedChart(Context context) {
+        super(context);
+    }
+
+    public XCJCombinedChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public XCJCombinedChart(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
