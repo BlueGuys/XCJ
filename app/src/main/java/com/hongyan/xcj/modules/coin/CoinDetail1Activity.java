01b9555d91644ed539b0a905753aaeb645cbd149
王宁 <wangninga@jiedaibao.com>
2018-03-30 09:57:56 +0800

    Fix 增加币种详情测试页

---
+++ app/src/main/java/com/hongyan/xcj/modules/coin/CoinDetail1Activity.java
@@ -0,0 +1,16 @@
+package com.hongyan.xcj.modules.coin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.hongyan.xcj.R;
+import com.hongyan.xcj.base.BaseActivity;
+
+public class CoinDetail1Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin);
+    }
+}
--- app/src/main/java/com/hongyan/xcj/modules/main/me/MeFragment.java
+++ app/src/main/java/com/hongyan/xcj/modules/main/me/MeFragment.java
@@ -15,6 +15,7 @@ import com.hongyan.xcj.base.BaseWebViewActivity;
 import com.hongyan.xcj.core.AccountInfo;
 import com.hongyan.xcj.core.AccountManager;
 import com.hongyan.xcj.core.AccountMessageEvent;
+import com.hongyan.xcj.modules.coin.CoinDetail1Activity;
 import com.hongyan.xcj.modules.coin.CoinDetailActivity;
 import com.hongyan.xcj.modules.collect.CollectActivity;
 import com.hongyan.xcj.widget.view.ItemCommonClickView;
@@ -70,6 +71,7 @@ public class MeFragment extends BaseFragment implements View.OnClickListener {
                 startActivity(new Intent(getActivity(), CollectActivity.class));
                 break;
             case R.id.linear_market:
+                startActivity(new Intent(getActivity(),CoinDetail1Activity.class));
                 break;
             case R.id.linear_clear:
                 startActivity(new Intent(getActivity(),CoinDetailActivity.class));
